65e843c498ffb050659d69790178f2564ea250b3
BlackDisc143 <55174450+BlackDisc143@users.noreply.github.com>
2020-03-29 14:36:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,8 +18,9 @@ async def sakebu(ctx):
     await ctx.send('Pythonわかんないいいいいいいいいいいいいいいいいいいいいいいいいいいいいい')
     
 @bot.event
-async def on_message(message):
-    if message == 'hoge':
-        await ctx.send('hage')
+async def on_member_update(before, after):
+    if before.status != after.status:
+        msg = after.display_name + " さんが " + str(after.status) + " になりました"
+        await client.send_message(text_chat,msg)
 
 bot.run(token)
