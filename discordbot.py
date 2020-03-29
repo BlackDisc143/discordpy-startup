7a913f41a2a42f42bf061cc7304c8f8c75906618
BlackDisc143 <55174450+BlackDisc143@users.noreply.github.com>
2020-03-29 14:57:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,16 +1,26 @@
-from discord.ext import commands
+#from discord.ext import commands
+import discord
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+#bot = commands.Bot(command_prefix='/')
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.command()
-async def chinato(ctx):
-    await ctx.send('私はチナトです。')
-    
-@bot.command()
-async def sakebu(ctx):
-    await ctx.send('Pythonわかんないいいいいいいいいいいいいいいいいいいいいいいいいいいいいい')
+#@bot.command()
+#async def chinato(ctx):
+#    await ctx.send('私はチナトです。')
+#    
+#@bot.command()
+#async def sakebu(ctx):
+#    await ctx.send('Pythonわかんないいいいいいいいいいいいいいいいいいいいいいいいいいいいいい')
+#    
+#bot.run(token)
 
-bot.run(token)
+@client.event
+async def on_message(message):
+    if message.content.startswith('/chinato'):
+        if client.user != message.author:
+            await message.channel.send('チナトです。')
+
+client.run("token")
