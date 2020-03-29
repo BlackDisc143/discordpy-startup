fd59fd23a9bf42d2e30ed6fadb0a972976007002
BlackDisc143 <55174450+BlackDisc143@users.noreply.github.com>
2020-03-29 14:45:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,10 +5,6 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
-async def on_ready():
-    await ctx.send('起動を確認したよ')
-
 @bot.command()
 async def chinato(ctx):
     await ctx.send('私はチナトです。')
@@ -16,11 +12,5 @@ async def chinato(ctx):
 @bot.command()
 async def sakebu(ctx):
     await ctx.send('Pythonわかんないいいいいいいいいいいいいいいいいいいいいいいいいいいいいい')
-    
-@bot.event
-async def on_member_update(before, after):
-    if before.status != after.status:
-        msg = after.display_name + " さんが " + str(after.status) + " になりました"
-        await client.send_message(text_chat,msg)
 
 bot.run(token)
